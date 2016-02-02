01b394fece98dbb49882560c7a6fabcbfb958371
nmbrodnax <nmbrodnax@gmail.com>
2016-02-02 09:16:46 -0500

    remove duplicates

--- demographics.py
+++ demographics.py
@@ -8,6 +8,7 @@ from def_split_email import *
 fieldnames = ['Status', 'Program', 'Field', 'Last', 'First', 'Gender',
               'Email2', 'Email1', 'Ethnicity']
 
+# Strip IU user IDs from email addresses and load into a list
 with open('all_students_sep15.csv', 'r') as master:
     reader = csv.DictReader(master, fieldnames)
     next(reader, None)  # skip the header row
@@ -48,7 +49,7 @@ with open('master_oct_15.csv', 'r', encoding='utf-8', errors='ignore') as \
     writer = csv.DictWriter(outfile, fieldnames_out)
     writer.writeheader()
     for student in reader:
-        temp = student.copy()
+        temp = student.copy()  # first name, last name, and email
         email_acct = student.get('email')
         email_name = split_email(student.get('email'))[0]
         email_domain = split_email(student.get('email'))[1]
@@ -56,6 +57,16 @@ with open('master_oct_15.csv', 'r', encoding='utf-8', errors='ignore') as \
                         student['ID'] == email_name.lower() or
                         student['Alt_Email'] == email_acct.lower()]
         if len(student_info) > 0:
+            # if the student is matched, remove their demographic information
+            # from the source list (student_list) so that if they are listed
+            # in the mailing list more than once with the same IUID, they will
+            # not also appear more than once in the final file.  This prevents
+            # duplicates for IU addresses (e.g. nbrodnax@indiana.edu and
+            # nbrodnax@umail.iu.edu are the same person but might both be
+            # subscribed).  There still might be duplicates for people with
+            # both IU and non-IU accounts but there is no way to verify a
+            # match between a non-IU email and a particular student
+            student_list.remove(student_info[0])
             for item in fields:
                 temp[item] = student_info[0][item]
         else:
